22a3eceb53aa7c2c40601aa443f614a942190736
AStark76 <46598570+AStark76@users.noreply.github.com>
2023-12-17 22:01:17 +0100

    Done
    Just testing

--- Daemon/Program.cs
+++ Daemon/Program.cs
@@ -1,22 +1,21 @@
 ﻿using Daemon.Model;
 namespace Ast.CleanDaemon.Daemon;
 
-public class Daemon
+public static class Daemon
 {
-    Process currentDuty;
+    
     private static void Main(string[] args)
     {
-        
+        Process currentDuty = null;
         if (0 == args.Length)
-        {
             DisplayHintMissingFile();
-        }
+        else if(2 == args.Length)
+            if ("-s" == args[1].ToLower()) currentDuty.IsSimulation = true;
         currentDuty = new Process(args[0]);
-        if ("-s" == args[1].ToLower()) currentDuty.IsSimulation = true;
-        Run(currentDuty);
+        PrepareRun(currentDuty);
     }
 
-    private static void Run(Process currentDuty)
+    private static void PrepareRun(Process currentDuty)
     {
         try
         {
@@ -24,12 +23,48 @@ public class Daemon
             foreach (var processPropertyLine in data)
             {
                 string[] taskProoperties = processPropertyLine.Split('|');
-
+                Run(taskProoperties, currentDuty);
             }
         }
         catch(Exception ex)
         {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Environment.Exit(-1);
+        }
+    }
 
+    private static void Run(string[] taskProoperties, Process inProcess)
+    {
+        string path = taskProoperties[0];
+        if (!int.TryParse(taskProoperties[1], out int month))
+        {
+            month = 6;
+        }
+        bool.TryParse(taskProoperties[1], out bool isSubdirectory);
+        if (inProcess.IsSimulation)
+            Delete(path, month, isSubdirectory, Console.WriteLine);
+        else
+            Delete(path, month, isSubdirectory, File.Delete);
+
+    }
+
+    private static void Delete(string path, int month, bool isSubdirectory, Action<string> inAction)
+    {
+        string[] files = Directory.GetFiles(path);
+        foreach (var item in files)
+        {
+            inAction(item);
+        }
+        if (isSubdirectory)
+        {
+            string[] directories = Directory.GetDirectories(path);
+            foreach (var d in directories)
+            {
+                Delete(d, month, isSubdirectory, inAction);
+            }
         }
     }
 
